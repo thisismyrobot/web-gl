a3f06d88cd6db566c0b0e77fb4d2cd16e0b32d5b
Robert Wallhead <rwallhead@gmail.com>
2009-09-02 19:43:43 +1000

    Swapped to useing a scrollable layout for the text - and fixed dpi/quality issues.

--- src/main.py
+++ src/main.py
@@ -8,14 +8,16 @@ class Page:
     """
 
     def __init__(self):
-        self.page = pyglet.text.Label('',
-                                      font_size=8,
-                                      width=400, 
-                                      height=300,
-                                      multiline=True, 
-                                      color=(0, 255, 0, 255),
-                                      anchor_x='center', 
-                                      anchor_y='center')
+        self.document = pyglet.text.decode_text('No Page Loaded')
+        self.document.set_style(0, 50, {'font-size':12, 'color':(0, 255, 0, 255)})
+        self.layout = pyglet.text.layout.ScrollableTextLayout(
+            self.document,
+            2000, 
+            1400,
+            dpi=200,
+            multiline=True)
+        self.layout.anchor_x='center' 
+        self.layout.anchor_y='center'
 
     def load_url(self, url):
         """Updates the self.text with the contents of a url
@@ -24,25 +26,25 @@ class Page:
         connection = urllib2.urlopen(request)
         data = connection.read()
         connection.close()
-        self.page.text = data
-
+        self.layout.document.text = data
+ 
     def draw(self):
 
         #draw text background
         pyglet.gl.glPushMatrix()
-        pyglet.gl.glTranslatef(0.0, 0.0, -500)
+        pyglet.gl.glTranslatef(0.0, 0.0, -2000)
         pyglet.gl.glColor4f(0, 1.0, 0, 1)
         pyglet.gl.glLineWidth(1.5);
         pyglet.gl.glBegin(pyglet.gl.GL_LINE_STRIP)
-        pyglet.gl.glVertex3f(-220.0, -170.0, 0.0)
-        pyglet.gl.glVertex3f(-220.0, 170.0, 0.0)
-        pyglet.gl.glVertex3f(220.0, 170.0, 0.0)
-        pyglet.gl.glVertex3f(220.0, -170.0, 0.0)
+        pyglet.gl.glVertex3f(-1050.0, -500.0, 0.0)
+        pyglet.gl.glVertex3f(-1050.0, 1000.0, 0.0)
+        pyglet.gl.glVertex3f(1100.0, 1000.0, 0.0)
+        pyglet.gl.glVertex3f(1100.0, -500.0, 0.0)
         pyglet.gl.glEnd()
         
         #draw text
-        pyglet.gl.glTranslatef(0.0, 0.0, 1)
-        self.page.draw()
+        pyglet.gl.glTranslatef(0.0, 250, 1)
+        self.layout.draw()
         pyglet.gl.glPopMatrix()
 
 
@@ -56,10 +58,10 @@ class Environment:
         pyglet.gl.glLineWidth(5);
         pyglet.gl.glColor4f(0.0, 0.0, 1.0, 0.1)
         pyglet.gl.glBegin(pyglet.gl.GL_LINE_LOOP)
-        pyglet.gl.glVertex3f(-550.0, -170.0, -550.0)
-        pyglet.gl.glVertex3f(-550.0, -170.0, 550.0)
-        pyglet.gl.glVertex3f(550.0, -170.0, 550.0)
-        pyglet.gl.glVertex3f(550.0, -170.0, -550.0)
+        pyglet.gl.glVertex3f(-2200.0, -500.0, -2200.0)
+        pyglet.gl.glVertex3f(-2200.0, -500.0, 2200.0)
+        pyglet.gl.glVertex3f(2200.0, -500.0, 2200.0)
+        pyglet.gl.glVertex3f(2200.0, -500.0, -2200.0)
         pyglet.gl.glEnd()
         pyglet.gl.glPopMatrix()
 
@@ -108,9 +110,11 @@ class Browser:
         self.render()
 
     def set_up_window(self):
-        self.window = pyglet.window.Window(resizable=True, fullscreen=True)
+        self.window = pyglet.window.Window(fullscreen=False, resizable=True)
         self.window.on_resize=self.camera.view
         self.window.on_mouse_drag=self.camera.drag
+        self.window.width=1024
+        self.window.height=768
         
     def opengl_init(self):
         pyglet.gl.glEnable(pyglet.gl.GL_BLEND)
@@ -135,7 +139,6 @@ class Browser:
     def draw_pages(self):
         self.page.draw()
 
-        
 Browser()
 
 
