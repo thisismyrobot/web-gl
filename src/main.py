319ef5d66ac5085023291160b440761bce70081c
Robert Wallhead <rwallhead@gmail.com>
2009-09-03 14:31:48 +1000

    Added mapping of mouse to cursor on page - doesn't handle rotation from default view.

--- src/main.py
+++ src/main.py
@@ -7,14 +7,19 @@ import pyglet.window
 class Page(object):
     """ Represents a remotely loaded page
     """
+    
+    layout_width = 2000 #visible text area width
+    layout_height = 1400 #visible text area height
+    page_distance = 2000 #from user
+    
     def __init__(self):
         style = {'font-size':14, 'color':(0, 255, 0, 255)}
         self.document = pyglet.text.decode_text('No Page Loaded')
         self.document.set_style(0, 50, style)
         self.layout = pyglet.text.layout.IncrementalTextLayout(
             self.document,
-            2000, 
-            1400,
+            self.layout_width, 
+            self.layout_height,
             dpi=200,
             multiline=True)
         self.layout.anchor_x='center' 
@@ -32,7 +37,7 @@ class Page(object):
     def draw_background(self):
         """ draw text background
         """
-        pyglet.gl.glTranslatef(0.0, 0.0, -2000)
+        pyglet.gl.glTranslatef(0.0, 0.0, -self.page_distance)
         pyglet.gl.glColor4f(0, 1.0, 0, 1)
         pyglet.gl.glLineWidth(1.5);
         pyglet.gl.glBegin(pyglet.gl.GL_LINE_STRIP)
@@ -98,7 +103,8 @@ class Camera(object):
     w,h=640,480
     far=8192
     fov=60
-
+    window=''
+    
     def view(self,width,height):
         self.w,self.h=width,height
         pyglet.gl.glViewport(0, 0, width, height)
@@ -130,6 +136,23 @@ class Camera(object):
         pyglet.gl.glRotatef(self.ry,0,1,0)
         pyglet.gl.glRotatef(self.rz,0,0,1)
 
+    def map_to_page(self, x, y):
+        """ Maps a screen click xy to a page xy
+        """
+        tx, ty = 0, 0
+
+        #fix origin based on screen size
+        tx = x - self.window.width/2
+        ty = y - self.window.height/2
+
+        #map to height/width ratios
+        tx = ((tx/(self.window.width/2.)) * 1850)
+        ty = ((ty/(self.window.height/2.)) * 1150) - 250
+
+        #print self.ry
+        
+        return tx, ty
+
 
 class Browser(object):
     """ Represents the application's main window - the browser.
@@ -140,6 +163,7 @@ class Browser(object):
 
         self.camera = Camera()
         self.set_up_window()
+        self.camera.window = self.window
         self.environment = Environment()
         self.page = Page()
         self.page.load_url("http://www.mightyseek.com/wp-content/plugins/podpress/readme.txt")
@@ -148,12 +172,15 @@ class Browser(object):
         self.render()
 
     def set_up_window(self):
-        self.window = pyglet.window.Window(fullscreen=True, resizable=True)
+        #self.window = pyglet.window.Window(fullscreen=True, resizable=True)
+        self.window = pyglet.window.Window(fullscreen=False, resizable=True)
         self.window.on_resize=self.camera.view
         self.window.on_mouse_drag=self.camera.drag
         self.window.on_mouse_scroll=self.scroll_page
-        #self.window.width=1280
-        #self.window.height=800
+        self.window.on_mouse_release=self.handle_mouse_up
+        self.window.on_mouse_motion=self.handle_mouse_up
+        self.window.width=1280
+        self.window.height=800
         #self.window.push_handlers(pyglet.window.event.WindowEventLogger())
 
     def opengl_init(self):
@@ -182,6 +209,15 @@ class Browser(object):
     def scroll_page(self, x, y, dx, dy):
         k = 100
         self.page.scroll(dy*k)
+        
+    #def handle_mouse_up(self, x, y, button, modifiers):
+    def handle_mouse_up(self, x, y, dx, dy):
+        #if button == 1:
+            x, y = self.camera.map_to_page(x, y)
+            line = self.page.layout.get_line_from_point(x, y)
+            char = self.page.layout.get_position_on_line(line, x)
+            self.page.layout.selection_background_color = (255, 0, 0, 255)
+            self.page.layout.set_selection(char, char+1)
 
 Browser()
 
