7d61508f2634177ed0ec5ec0be2c6e2e7f15ac7a
Robert Wallhead <rwallhead@gmail.com>
2009-09-03 10:02:29 +1000

    Tiny whitespace etc fixes

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ import pyglet.window
 class Page(object):
     """ Represents a remotely loaded page
     """
-
     def __init__(self):
         style = {'font-size':14, 'color':(0, 255, 0, 255)}
         self.document = pyglet.text.decode_text('No Page Loaded')
@@ -50,13 +49,11 @@ class Page(object):
         pyglet.gl.glTranslatef(0.0, 250, 1)
         self.layout.draw()
         pyglet.gl.glPopMatrix()
-        
+
     def draw_slider(self):
         """ draw slider position
         """
         pyglet.gl.glTranslatef(1100, 1000, 0.0)
-
-        #deterimine the scroll position
         scroll_pos = (float(self.layout.view_y) / float(self.layout.content_height - 1400)) * 1500
         pyglet.gl.glTranslatef(0.0, scroll_pos, 0.0)
         pyglet.gl.glColor4f(0, 1.0, 0, 1)
@@ -67,12 +64,12 @@ class Page(object):
         pyglet.gl.glVertex3f(10.0, 10.0, 0.0)
         pyglet.gl.glVertex3f(10.0, -10.0, 0.0)
         pyglet.gl.glEnd()
-    
+
     def draw(self):
         self.draw_background()
         self.draw_text()
         self.draw_slider()
-        
+
     def scroll(self, s):
         self.layout.view_y += s
 
@@ -96,6 +93,7 @@ class Environment(object):
 
 
 class Camera(object):
+
     rx,ry,rz=0,0,0
     w,h=640,480
     far=8192
@@ -112,11 +110,11 @@ class Camera(object):
     def drag(self, x, y, dx, dy, button, modifiers):
         if button==4:
             self.update_r(dx, dy)
-            
+
     def update_r(self, dx, dy):
         new_rx = self.rx-(dy/4.0)
         new_ry = self.ry+(dx/4.0)
-        
+
         if new_rx < -30:
             new_rx = -30
 
@@ -125,7 +123,7 @@ class Camera(object):
 
         self.rx=new_rx
         self.ry=new_ry
-            
+
     def apply(self):
         pyglet.gl.glLoadIdentity()
         pyglet.gl.glRotatef(self.rx,1,0,0)
@@ -136,11 +134,10 @@ class Camera(object):
 class Browser(object):
     """ Represents the application's main window - the browser.
     """
-    
     def __init__(self):
         """ sets up the the window, starts rendering the browser
         """
-        
+
         self.camera = Camera()
         self.set_up_window()
         self.environment = Environment()
