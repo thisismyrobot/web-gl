df65409c8feeb3fe3a00bc167ec7eb883baa7c38
Robert Wallhead <rwallhead@gmail.com>
2009-09-03 15:45:20 +1000

    Major rework of code/idea.
    Now aiming for layered windows - but super-cool vector style. Parameterised a ton of the code too.

--- src/main.py
+++ src/main.py
@@ -2,15 +2,17 @@ import urllib2
 import pyglet.gl
 import pyglet.text
 import pyglet.window
+import math
 
 
 class Page(object):
     """ Represents a remotely loaded page
     """
     
-    layout_width = 2000 #visible text area width
-    layout_height = 1400 #visible text area height
+    page_width = 1600 #visible text area width
+    page_height = 2000 #visible text area height
     page_distance = 2000 #from user
+    page_border = 50 #spacing around text
     
     def __init__(self):
         style = {'font-size':14, 'color':(0, 255, 0, 255)}
@@ -18,8 +20,8 @@ class Page(object):
         self.document.set_style(0, 50, style)
         self.layout = pyglet.text.layout.IncrementalTextLayout(
             self.document,
-            self.layout_width, 
-            self.layout_height,
+            self.page_width, 
+            self.page_height,
             dpi=200,
             multiline=True)
         self.layout.anchor_x='center' 
@@ -40,26 +42,26 @@ class Page(object):
         pyglet.gl.glTranslatef(0.0, 0.0, -self.page_distance)
         pyglet.gl.glColor4f(0, 1.0, 0, 1)
         pyglet.gl.glLineWidth(1.5);
-        pyglet.gl.glBegin(pyglet.gl.GL_LINE_STRIP)
-        pyglet.gl.glVertex3f(-1050.0, -500.0, 0.0)
-        pyglet.gl.glVertex3f(-1050.0, 1000.0, 0.0)
-        pyglet.gl.glVertex3f(1100.0, 1000.0, 0.0)
-        pyglet.gl.glVertex3f(1100.0, -500.0, 0.0)
+        pyglet.gl.glBegin(pyglet.gl.GL_LINE_LOOP)
+        pyglet.gl.glVertex3f((-self.page_width/2)-self.page_border, (-self.page_height/2)-self.page_border, 0.0)
+        pyglet.gl.glVertex3f((-self.page_width/2)-self.page_border, (self.page_height/2)+self.page_border, 0.0)
+        pyglet.gl.glVertex3f((self.page_width/2)+self.page_border, (self.page_height/2)+self.page_border, 0.0)
+        pyglet.gl.glVertex3f((self.page_width/2)+self.page_border, (-self.page_height/2)-self.page_border, 0.0)
         pyglet.gl.glEnd()
 
     def draw_text(self):
         """ draw text
         """
         pyglet.gl.glPushMatrix()
-        pyglet.gl.glTranslatef(0.0, 250, 1)
+        pyglet.gl.glTranslatef(0.0, 0.0, 1.0)
         self.layout.draw()
         pyglet.gl.glPopMatrix()
 
     def draw_slider(self):
         """ draw slider position
         """
-        pyglet.gl.glTranslatef(1100, 1000, 0.0)
-        scroll_pos = (float(self.layout.view_y) / float(self.layout.content_height - 1400)) * 1500
+        pyglet.gl.glTranslatef((self.page_width/2)+self.page_border, (self.page_height/2)+self.page_border, 0.0)
+        scroll_pos = (float(self.layout.view_y) / float(self.layout.content_height - self.page_height)) * (self.page_height + self.page_border * 2)
         pyglet.gl.glTranslatef(0.0, scroll_pos, 0.0)
         pyglet.gl.glColor4f(0, 1.0, 0, 1)
         pyglet.gl.glLineWidth(1.5);
@@ -82,7 +84,8 @@ class Page(object):
 class Environment(object):
 
     def draw(self):
-        self.draw_base()
+        #self.draw_base()
+        pass
 
     def draw_base(self):
         pyglet.gl.glPushMatrix()
@@ -103,7 +106,6 @@ class Camera(object):
     w,h=640,480
     far=8192
     fov=60
-    window=''
     
     def view(self,width,height):
         self.w,self.h=width,height
@@ -144,18 +146,40 @@ class Camera(object):
         #fix origin based on screen size
         tx = x - self.window.width/2
         ty = y - self.window.height/2
-
+        
         #map to height/width ratios
         tx = ((tx/(self.window.width/2.)) * 1850)
         ty = ((ty/(self.window.height/2.)) * 1150) - 250
-
-        #print self.ry
         
         return tx, ty
 
 
-class Browser(object):
-    """ Represents the application's main window - the browser.
+class Pages(object):
+    """ Represents the loaded pages
+    """
+    pages = []
+    
+    def add_page(self, url):
+        new_page = Page()
+        new_page.load_url(url)
+        self.pages.append(new_page)
+
+    def draw(self):
+        for page in self.pages:
+            page.draw()
+            
+    @property
+    def focussed_page(self):
+        return self.pages[0]
+
+
+class ControlPanel(object):
+    """ Represents the hovering control area
+    """
+
+
+class Desktop(object):
+    """ Represents the app - containing pages with content
     """
     def __init__(self):
         """ sets up the the window, starts rendering the browser
@@ -165,22 +189,23 @@ class Browser(object):
         self.set_up_window()
         self.camera.window = self.window
         self.environment = Environment()
-        self.page = Page()
-        self.page.load_url("http://www.mightyseek.com/wp-content/plugins/podpress/readme.txt")
-        
+        self.pages = Pages()
+        self.pages.add_page("http://www.mightyseek.com/wp-content/plugins/podpress/readme.txt")
         self.opengl_init()
         self.render()
 
     def set_up_window(self):
-        #self.window = pyglet.window.Window(fullscreen=True, resizable=True)
-        self.window = pyglet.window.Window(fullscreen=False, resizable=True)
+        self.window = pyglet.window.Window(fullscreen=True, resizable=True)
+        
+        #self.window = pyglet.window.Window(fullscreen=False, resizable=True)
+        #self.window.width=1280
+        #self.window.height=800
+        
         self.window.on_resize=self.camera.view
-        self.window.on_mouse_drag=self.camera.drag
+        #self.window.on_mouse_drag=self.camera.drag
         self.window.on_mouse_scroll=self.scroll_page
-        self.window.on_mouse_release=self.handle_mouse_up
-        self.window.on_mouse_motion=self.handle_mouse_up
-        self.window.width=1280
-        self.window.height=800
+        #self.window.on_mouse_release=self.handle_mouse_up
+        #self.window.on_mouse_motion=self.handle_mouse_up
         #self.window.push_handlers(pyglet.window.event.WindowEventLogger())
 
     def opengl_init(self):
@@ -196,30 +221,23 @@ class Browser(object):
             pyglet.gl.glClear(pyglet.gl.GL_COLOR_BUFFER_BIT | pyglet.gl.GL_DEPTH_BUFFER_BIT)
             self.camera.apply()
             self.environment.draw()
-            self.page.draw()
+            self.pages.draw()
             self.window.flip()
-
-    def add_page(self, url):
-        self.page = Page()
-        self.page.load_url(url)
-
-    def draw_pages(self):
-        self.page.draw()
 
     def scroll_page(self, x, y, dx, dy):
         k = 100
-        self.page.scroll(dy*k)
+        self.pages.focussed_page.scroll(dy*k)
         
-    #def handle_mouse_up(self, x, y, button, modifiers):
-    def handle_mouse_up(self, x, y, dx, dy):
-        #if button == 1:
-            x, y = self.camera.map_to_page(x, y)
-            line = self.page.layout.get_line_from_point(x, y)
-            char = self.page.layout.get_position_on_line(line, x)
-            self.page.layout.selection_background_color = (255, 0, 0, 255)
-            self.page.layout.set_selection(char, char+1)
+#    def handle_mouse_up(self, x, y, button, modifiers):
+#    def handle_mouse_up(self, x, y, dx, dy):
+#        if button == 1:
+#            x, y = self.camera.map_to_page(x, y)
+#            line = self.page.layout.get_line_from_point(x, y)
+#            char = self.page.layout.get_position_on_line(line, x)
+#            self.page.layout.selection_background_color = (255, 0, 0, 255)
+#            self.page.layout.set_selection(char, char+1)
 
-Browser()
+Desktop()
 
 
 
