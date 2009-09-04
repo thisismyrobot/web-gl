ee279cf3c016af8feedc4a15cb36426a8ae304d9
Robert Wallhead <rwallhead@gmail.com>
2009-09-04 13:56:30 +1000

    Added keyboard controls WASD
    Also a screenshots folder.

--- src/main.py
+++ src/main.py
@@ -5,10 +5,29 @@ import pyglet.window
 import math
 import random
 
+
+class Keys(object):
+    """ Stores the state of keys
+    """
+    keys = []
+    
+    @classmethod
+    def down(self, symbol, modifiers):
+        """ Adds a key to the array, signifying that it is being pressed.
+        """
+        self.keys.append(symbol)
+        print "up %s" % symbol
+
+    @classmethod
+    def up(self, symbol, modifiers):
+        """ Adds a key to the array, signifying that it is being pressed.
+        """
+        self.keys.remove(symbol)
+
+
 class Page(object):
     """ Represents a remotely loaded page
     """
-    
     page_width = 1600 #visible text area width
     page_height = 2000 #visible text area height
     page_distance = 2000 #from user
@@ -18,12 +37,11 @@ class Page(object):
     page_alpha_focussed = 1.0
     page_alpha_unfocussed = 0.25
 
-    page_alpha = page_alpha_unfocussed
+    page_alpha = page_alpha_focussed
     
-    page_x_offset = 0
-    page_y_offset = 0
-    page_z_offset = 0
-    page_z_rotation = 0    
+    x,y,z,rz = 0,0,0,0
+    
+    r,g,b = 0.0,1.0,0.0
     
     def __init__(self):
         self.document = pyglet.text.decode_text('No Page Loaded')
@@ -45,9 +63,9 @@ class Page(object):
         self.update_document_style()
 
     def update_document_style(self):
-        style = {'font-size':self.page_font_size, 'color':(0, 255, 0, int(self.page_alpha*255))}
+        style = {'font-size':self.page_font_size, 'color':(int(self.r*255), int(self.g*255), int(self.b*255), int(self.page_alpha*255))}
         self.document.set_style(0, 50, style)
-            
+
     def load_url(self, url):
         """Updates the self.text with the contents of a url
         """
@@ -61,7 +79,7 @@ class Page(object):
         """ draw text background
         """
         pyglet.gl.glTranslatef(0.0, 0.0, -self.page_distance)
-        pyglet.gl.glColor4f(0, 1.0, 0, self.page_alpha)
+        pyglet.gl.glColor4f(self.r, self.g, self.b, self.page_alpha)
         pyglet.gl.glLineWidth(1.5);
         pyglet.gl.glBegin(pyglet.gl.GL_LINE_LOOP)
         pyglet.gl.glVertex3f((-self.page_width/2)-self.page_border, (-self.page_height/2)-self.page_border, 0.0)
@@ -85,21 +103,24 @@ class Page(object):
         scroll_pos = (float(self.layout.view_y) / float(self.layout.content_height - self.page_height)) * (self.page_height + self.page_border * 2)
         pyglet.gl.glTranslatef(0.0, scroll_pos, 0.0)
         pyglet.gl.glLineWidth(1.5)
-        pyglet.gl.glColor4f(0, 1.0, 0, self.page_alpha)
+        pyglet.gl.glColor4f(self.r, self.g, self.b, self.page_alpha)
         pyglet.gl.glBegin(pyglet.gl.GL_LINE_LOOP)
-        pyglet.gl.glVertex3f(-10.0, -10.0, 0.0)
-        pyglet.gl.glVertex3f(-10.0, 10.0, 0.0)
-        pyglet.gl.glVertex3f(10.0, 10.0, 0.0)
-        pyglet.gl.glVertex3f(10.0, -10.0, 0.0)
+        pyglet.gl.glVertex3f(-20.0, -20.0, 0.0)
+        pyglet.gl.glVertex3f(-20.0, 20.0, 0.0)
+        pyglet.gl.glVertex3f(20.0, 20.0, 0.0)
+        pyglet.gl.glVertex3f(20.0, -20.0, 0.0)
         pyglet.gl.glEnd()
 
     def scroll(self, s):
         self.layout.view_y += s
 
+    def position_page(self):
+        pyglet.gl.glTranslatef(self.x, self.y, self.z)
+        pyglet.gl.glRotatef(self.rz, 0, 0, 1)
+
     def draw(self):
         pyglet.gl.glPushMatrix()
-        pyglet.gl.glTranslatef(self.page_x_offset, self.page_y_offset, self.page_z_offset)
-        pyglet.gl.glRotatef(self.page_z_rotation, 0, 0, 1)
+        self.position_page()
         self.draw_background()
         self.draw_text()
         self.draw_slider()
@@ -131,6 +152,8 @@ class Camera(object):
     w,h=640,480
     far=8192
     fov=60
+    x,y,z=0,0,0
+    speed=10
     
     def view(self,width,height):
         self.w,self.h=width,height
@@ -140,9 +163,8 @@ class Camera(object):
         pyglet.gl.gluPerspective(self.fov, float(self.w)/self.h, 0.1, self.far)
         pyglet.gl.glMatrixMode(pyglet.gl.GL_MODELVIEW)
 
-    def drag(self, x, y, dx, dy, button, modifiers):
-        if button==4:
-            self.update_r(dx, dy)
+    def drag(self, x, y, dx, dy):
+        self.update_r(dx, dy)
 
     def update_r(self, dx, dy):
         new_rx = self.rx-(dy/4.0)
@@ -157,27 +179,30 @@ class Camera(object):
         self.rx=new_rx
         self.ry=new_ry
 
+    def move(self):
+        """ Iterates the key states, applying movement transforms as needed.
+        """
+        for key in Keys.keys:
+            if key is 119: #W - forward
+                self.x -= math.sin(math.radians(self.ry)) * self.speed
+                self.z += math.cos(math.radians(self.ry)) * self.speed
+            if key is 115: #S - backward
+                self.x += math.sin(math.radians(self.ry)) * self.speed
+                self.z -= math.cos(math.radians(self.ry)) * self.speed
+            if key is 97: #A - strafe left
+                self.x += math.cos(math.radians(self.ry)) * self.speed
+                self.z += math.sin(math.radians(self.ry)) * self.speed
+            if key is 100:
+                self.x -= math.cos(math.radians(self.ry)) * self.speed
+                self.z -= math.sin(math.radians(self.ry)) * self.speed
+
     def apply(self):
+        self.move()
         pyglet.gl.glLoadIdentity()
         pyglet.gl.glRotatef(self.rx,1,0,0)
         pyglet.gl.glRotatef(self.ry,0,1,0)
         pyglet.gl.glRotatef(self.rz,0,0,1)
-
-    def map_to_page(self, x, y):
-        """ Maps a screen click xy to a page xy
-        """
-        tx, ty = 0, 0
-
-        #fix origin based on screen size
-        tx = x - self.window.width/2
-        ty = y - self.window.height/2
-        
-        #map to height/width ratios
-        tx = ((tx/(self.window.width/2.)) * 1850)
-        ty = ((ty/(self.window.height/2.)) * 1150) - 250
-        
-        return tx, ty
-
+        pyglet.gl.glTranslatef(self.x, self.y, self.z)
 
 class Pages(object):
     """ Represents the loaded pages
@@ -185,27 +210,19 @@ class Pages(object):
     pages = []
 
     def add_page(self, url):
+        if len(self.pages) > 0:
+            self.pages[0].set_focussed(False)
         new_page = Page()
         new_page.load_url(url)
-        if len(self.pages) > 0:
-            new_page.page_z_offset = self.pages[len(self.pages)-1].page_z_offset - 100
-            new_page.page_z_rotation = (random.random()*30)-15
-            new_page.page_x_offset = (random.random()*2000)-1000
-            new_page.page_y_offset = (random.random()*100)-50
-        self.pages.append(new_page)
+        self.pages.insert(0, new_page)
 
     def draw(self):
         pyglet.gl.glPushMatrix()
-        
-        self.focussed_page.set_focussed(True)
-        self.focussed_page.draw()
-        
-        for page in self.pages[1:]:
-            page.set_focussed(False)
-            page.draw()
-        
+        for i in range(len(self.pages)):
+            self.pages[i].z = i * -100
+            self.pages[i].draw()
         pyglet.gl.glPopMatrix()
-        
+
     @property
     def focussed_page(self):
         return self.pages[0]
@@ -236,14 +253,20 @@ class Desktop(object):
     def set_up_window(self):
         #self.window = pyglet.window.Window(fullscreen=True, resizable=True)
         
+        #set up window
         self.window = pyglet.window.Window(fullscreen=False, resizable=True)
         self.window.width=1280
         self.window.height=800
         
+        #handlers
         self.window.on_resize=self.camera.view
-        #self.window.on_mouse_drag=self.camera.drag
+        self.window.on_mouse_motion=self.camera.drag
         self.window.on_mouse_scroll=self.scroll_page
-        #self.window.on_mouse_release=self.handle_mouse_up
+        self.window.on_mouse_release=self.handle_mouse_up
+        self.window.on_mouse_press=self.handle_mouse_down
+        self.window.on_key_press=Keys.down
+        self.window.on_key_release=Keys.up
+
         #self.window.on_mouse_motion=self.handle_mouse_up
         #self.window.push_handlers(pyglet.window.event.WindowEventLogger())
 
@@ -266,15 +289,37 @@ class Desktop(object):
     def scroll_page(self, x, y, dx, dy):
         k = 100
         self.pages.focussed_page.scroll(dy*k)
+
+    def handle_mouse_up(self, x, y, button, modifiers):
+        if button == 1:
+            x, y = self.map_to_page(x, y)
+            line = self.pages.focussed_page.layout.get_line_from_point(x, y)
+            char = self.pages.focussed_page.layout.get_position_on_line(line, x)
+            self.pages.focussed_page.layout.selection_background_color = (255, 0, 0, 255)
+            self.pages.focussed_page.layout.set_selection(char, char+1)
+
+    def handle_mouse_down(self, x, y, button, modifiers):
+        if button == 1:
+            x, y = self.map_to_page(x, y)
+            if x < 900 and x > -900 and y < 1050 and y > -1050:
+                self.pages.focussed_page.r=1
+                self.pages.focussed_page.g=0
+
+    def map_to_page(self, x, y):
+        """ Maps a screen click xy to a page xy
+        """
+        tx, ty = 0, 0
+
+        #fix origin based on screen size
+        tx = x - self.window.width/2
+        ty = y - self.window.height/2
         
-#    def handle_mouse_up(self, x, y, button, modifiers):
-#    def handle_mouse_up(self, x, y, dx, dy):
-#        if button == 1:
-#            x, y = self.camera.map_to_page(x, y)
-#            line = self.page.layout.get_line_from_point(x, y)
-#            char = self.page.layout.get_position_on_line(line, x)
-#            self.page.layout.selection_background_color = (255, 0, 0, 255)
-#            self.page.layout.set_selection(char, char+1)
+        #map to height/width ratios
+        tx = (tx/(self.window.width/2.)) * 2050
+        ty = (ty/(self.window.height/2.)) * 1150
+        
+        return tx, ty
+
 
 Desktop()
 
