1fddb5f836180d9bb232f0c432b55ad681ab1a8d
Robert Wallhead <rwallhead@gmail.com>
2009-09-05 14:34:07 +1000

    Added first doc-tests to application.

---
+++ src/runtests.py
@@ -0,0 +1,9 @@
+""" Basic doc-tests for Web-GL
+"""
+import doctest
+import os
+
+doctest.testfile("tests.txt", verbose=True, optionflags=doctest.ELLIPSIS)
+
+#hack to pause after running script - on windoze testfile causes a loss of focus for some reason...
+raw_input("Focus this console and press Enter to close")
